187d3df79e2c71f2873d07a61b78378c49fcd182
Wenderson Amaro De Souza <wenderson3souza@gmail.com>
2023-07-03 12:54:01 -0400

    make request

--- lib/api_abstraction/api.py
+++ lib/api_abstraction/api.py
@@ -4,3 +4,6 @@ from .base_api import BaseApi
 class Api(BaseApi):
     def __init__(self, name, scheme, authority):
         super.__init__(name, scheme, authority)
+    
+    def request(self, method, endpoint, params):
+        response = request[method](endpoint.get_uri(), params=params)
