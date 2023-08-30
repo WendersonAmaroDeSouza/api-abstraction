860f2a0ea5772d38591b03e916b9d95d7b5eda28
Wenderson Amaro De Souza <wenderson3souza@gmail.com>
2023-08-30 09:59:53 -0400

    add cookies

--- lib/api_abstraction/src/api_abstraction_capiv/api.py
+++ lib/api_abstraction/src/api_abstraction_capiv/api.py
@@ -2,8 +2,8 @@ import requests
 from .base_api import BaseApi
 
 class Api(BaseApi):
-    def __init__(self, name, scheme, authority):
-        super().__init__(name, scheme, authority)
+    def __init__(self, name, scheme, authority, response_transformers):
+        super().__init__(name, scheme, authority, response_transformers)
     
     def request(self, endpoint, headers, params):
         response = endpoint.send_request(headers, params)
--- lib/api_abstraction/src/api_abstraction_capiv/base_api.py
+++ lib/api_abstraction/src/api_abstraction_capiv/base_api.py
@@ -1,15 +1,19 @@
 from enum import Enum
+from types import SimpleNamespace
 import requests
 
 class BaseApi:
-    def __init__(self, name, scheme, authority):
+    def __init__(self, name, scheme, authority, transformers):
         self.name = name
         self.scheme = scheme
         self.authority = authority
+        self.transformers = transformers
         self._resources = []
+        self.resources = SimpleNamespace()
     
     def add_resource(self, resource):
         self._resources.append(resource)
+        setattr(self.resources, resource.name, resource)
         resource.api = self
 
 class Resource:
@@ -18,16 +22,35 @@ class Resource:
         self.api = api
         api.add_resource(self)
         self._endpoints = []
+        self.endpoints = SimpleNamespace()
     
     def add_endpoint(self, endpoint):
         self._endpoints.append(endpoint)
+        setattr(self.endpoints, endpoint.name, endpoint)
         endpoint.resource = self
 
 class Endpoint:
-    def __init__(self, path, http_method, resource):
+    def __init__(
+            self, 
+            path, 
+            name, 
+            http_method, 
+            resource, 
+            default_headers=None, 
+            default_params=None,
+            default_data=None,
+            default_cookies=None, 
+            response_transforme=False):
         self.path = path
+        self.name = name
         self.http_method = http_method
         self.resource = resource
+        self.default_headers = default_headers
+        self.default_params = default_params
+        self.default_data = default_data
+        self.default_cookies = default_cookies
+        self.response_transform = response_transforme
+        self.transformers = self.resource.api.transformers
         resource.add_endpoint(self)
     
     def get_uri(self):
@@ -36,9 +59,21 @@ class Endpoint:
         authority = api.authority
         return f'{scheme}{authority}{self.path}'
     
-    def send_request(self, headers, params):
-        response = getattr(requests, self.http_method.value)(self.get_uri())
+    def send_request(self, headers=None, params=None, data=None, cookies=None):
+        headers = headers or self.default_headers
+        params = params or self.default_params
+        data = data or self.default_data
+        cookies = cookies or self.default_cookies
+        response = getattr(requests, self.http_method.value)(self.get_uri(), headers=headers, params=params, data=data, cookies=cookies)
         print(f'{response.status_code}: [{self.http_method.value}] {self.get_uri()}')
+        if self.response_transform:
+            response = self.__transform_response(response)
+        return response
+    
+    def __transform_response(self, response):
+        resource_transforms = getattr(self.transformers, self.resource.name)
+        transform = getattr(resource_transforms, self.name)
+        response = transform(response)
         return response
 
 class HTTPMethod(Enum):
